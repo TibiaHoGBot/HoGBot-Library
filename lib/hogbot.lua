8ed6fa1209ba49aeccc32d3bdd614e108093de32
Radoslaw Sroka <sroka.radoslaw92@gmail.com>
2023-12-13 00:54:48 +0100

    some functions

--- lib/hogbot.lua
+++ lib/hogbot.lua
@@ -15,23 +15,6 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
--- @name    	distance
--- @desc    	distance between two points
--- @author     	spec8320
--- @returns     number
-
-function distance(x1, y1, x2, y2)
-    -- Check if all arguments are numbers
-    if type(x1) ~= "number" or type(y1) ~= "number" or type(x2) ~= "number" or type(y2) ~= "number" then
-        error("All arguments must be numbers")
-    end
-    
-    -- Calculate the distance using the Euclidean formula
-    local distance = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
-    
-    return distance
-end
-
 --[[
         Constants
 --]]
@@ -107,6 +90,23 @@ STEP_DIRECTION_SOUTH_EAST   = 8
         User functions
 --]]
 
+-- @name    	distance
+-- @desc    	distance between two points
+-- @author     	spec8320
+-- @returns     number
+
+function distance(x1, y1, x2, y2)
+    -- Check if all arguments are numbers
+    if type(x1) ~= "number" or type(y1) ~= "number" or type(x2) ~= "number" or type(y2) ~= "number" then
+        error("All arguments must be numbers")
+    end
+    
+    -- Calculate the distance using the Euclidean formula
+    local distance = math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    
+    return distance
+end
+
 -- @name    hppc
 -- @desc    amount of current health percent
 -- @author  Dworak
@@ -139,6 +139,215 @@ function mppc()
     else
         return 0
     end
+end
+
+-- @name    hasitem
+-- @desc    check if player has any item by id
+-- @author  dulec
+-- @returns boolean
+function hasitem(itemid)
+    if type(itemid) ~= "number" then
+        error("itemid must be number")
+    end
+
+    local containers = getcontainers()
+    
+    for _, container in ipairs(containers) do
+        for _, item in ipairs(container.items) do
+            if item.id == itemid then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+-- @name    getitemsontile
+-- @desc    get all items on specific tile
+-- @author  dulec
+-- @returns array
+function getitemsontile(position)
+    if getmetatable(position) ~= Position then
+        error('position must be Position')
+    end
+
+    local tiles = gettiles()
+    for _, tile in ipairs(tiles) do
+        if tile.position.x == position.x and tile.position.y == position.y then
+            return tile
+        end
+    end
+end
+
+-- @name    moveallitemstoyourposition
+-- @desc    move all items from position to your position 
+-- @author  dulec
+-- @returns void
+function moveallitemstoyourposition(position)
+    if getmetatable(position) ~= Position then
+        error('position must be Position')
+    end
+
+    local itemsToMove = getItemsOnTile(position)
+    local selfPosition = selfposition()
+
+    while #itemsToMove > 0 do
+        moveobject(position, itemsToMove[-1], #itemsToMove, selfPosition, itemsToMove[-1].count)  
+        table.remove(itemsToMove, itemsToMove[-1]) 
+    end
+end
+
+-- @name    moveallitemsonground
+-- @desc    move all items from position to destination
+-- @author  dulec
+-- @returns void
+function moveallitemsonground(position, destination)
+    if getmetatable(position) ~= Position or getmetatable(destination) ~= Position then
+        error('All arguments must be Positions')
+    end
+
+    local itemsToMove = getItemsOnTile(position)
+    itemsToMove = table.remove(itemsToMove, 0)
+    local selfPosition = selfposition()
+
+    while #itemsToMove > 0 do
+        moveobject(position, itemsToMove[-1], #itemsToMove, selfPosition, itemsToMove[-1].count) 
+        table.remove(itemsToMove, itemsToMove[-1]) 
+    end
+end
+
+-- @name    moveitemonground
+-- @desc    move items from position to destination
+-- @author  dulec
+-- @returns void
+function moveitemonground(position, destination, itemid, amount)
+    if type(itemid) ~= "number" or type(amount) ~= "number" then
+        error("itemid and amount must be numbers")
+    end 
+
+    if getmetatable(position) ~= Position or getmetatable(destination) ~= Position then
+        error('position and destination arguments must be Positions')
+    end
+
+    while amount > 0 then
+        if amount >= 100 then
+            moveobject(position, itemid, 0, destination, 100)
+            amount -= 100 
+        else
+            moveobject(position, itemid, 0, destination, 100-amount) 
+            amount = 0
+        end
+    end
+end
+
+-- @name    finditemindex
+-- @desc    find item index in items array
+-- @author  dulec
+-- @returns number
+function finditemindex(itemlist, itemid)
+    if type(itemid) ~= "number" then
+        error("itemid must be number")
+    end
+
+    if type(itemlist) ~= "table" then
+        error("itemlist must be table")
+    end
+
+    if #itemlist < 1 then
+        return -1
+    end
+
+    for index, item in ipairs(itemlist) do
+        if item.id == itemid then
+            return index
+        end
+    end
+
+    return -1
+end
+
+-- @name    countitems
+-- @desc    amount of items in cointainers by id
+-- @author  dulec
+-- @returns number
+function countitems(itemid)
+    if type(itemid) ~= "number" then
+        error("All arguments must be numbers")
+    end 
+
+    local containers = getcontainers()
+    local count = 0
+    
+    for _, container in ipairs(containers) do
+        for _, item in ipairs(container.items) do
+            if item.id == id then
+                count += item.count
+            end
+        end
+    end
+    return count
+end
+
+-- @name    getitempositionfromcontainers
+-- @desc    search all your containers until find first item with itemid and returns its position
+-- @author  dulec
+-- @returns position
+function getitempositionfromcontainers(itemid)
+    if type(itemid) ~= "number" then
+        error("All arguments must be numbers")
+    end 
+
+    local containers = getcontainers()
+    for i, container in ipairs(containers) do
+        for j, item in ipairs(container.items) do
+            if item == itemid then
+                return Position:new(0xffff, 0x40 + i,  j)
+            end
+        end
+    end
+end
+
+-- @name    dropitems
+-- @desc    drop specific items on floor, position optional(if nil will drop on self)
+-- @author  dulec
+-- @returns void
+function dropitems(itemid, amount, position)
+    if type(itemid) ~= "number" or type(amount) ~= "number" and type(amount) ~= nil then
+        error("All arguments must be numbers")
+    end 
+
+    if position == nil then
+        position = selfposition()
+    end
+
+    local itemscount = countitems(itemid)
+    if itemscount < amount then
+        amount = itemscount
+    end
+
+    while amount > 0 then
+        local itemposition = getitempositionfromcontainers(itemid)
+
+        if(itemposition == nil) then
+            return
+        end
+
+        if amount >= 100 then
+            moveobject(itemposition, itemid, 0, selfposition, 100)
+            amount -= 100 
+        else
+            moveobject(itemposition, itemid, 0, selfposition, 100-amount) 
+            amount = 0
+        end
+    end
+end
+
+-- @name    selfposition
+-- @desc    return self position
+-- @author  dulec
+-- @returns Position
+function selfposition()
+    return Position:new(posx(), posy(),  posz())
 end
 
 --[[
