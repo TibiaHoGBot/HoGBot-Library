4f879488ff7e6faf78e4232577256a7cb1c6f37d
DvScripts <57599756+DvScripts@users.noreply.github.com>
2024-02-29 15:08:05 +0100

    Update hogbot.lua
    cancast fix

--- lib/hogbot.lua
+++ lib/hogbot.lua
@@ -2651,7 +2651,7 @@ function cancast(spellName)
     end
 
     local cooldownId, minMana, spellGroup = getspelldetails(spellName)
-    return cooldown(cooldownId) and cooldowngroup(spellGroup) and mp() >= minMana
+    return (not cooldown(cooldownId)) and (not cooldowngroup(spellGroup)) and mp() >= minMana
 end
 
 --- casts spell
